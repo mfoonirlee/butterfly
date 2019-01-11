a9891064d437176f46bb74a4843f913ce630aa15
mfoonirlee <745594100@163.com>
2019-01-11 19:53:08 +0800

    Update butterfly.js

--- dist/butterfly.js
+++ dist/butterfly.js
@@ -130,6 +130,11 @@ window.onload = function() {
 
   var timer = setInterval(function () {
     if (counter++ < 50) {
+        if(counter > 3){
+            var div = document.createElement('div');
+            div.innerHTML = '<div class="text-words"><p class="fsn">Fate/stay night</p><p class="mid white">Heaven\'s Feel</p><p class="mid white">presage flower</p><p class="sm red slowAni">2019&nbsp.&nbsp1&nbsp.&nbsp11</p><p class="mid red slowAni">on view</p></div><p class="fadeIn">Made By @boltCat</p>';
+            document.body.appendChild(div);
+        }
         new Butterfly();
     } else {
         clearInterval(timer);
